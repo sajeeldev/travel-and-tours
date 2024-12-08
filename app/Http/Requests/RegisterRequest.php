686222c0ba10ef62f86946900ad700e995a6465b
sajeeldev <sajeelwebdev@gmail.com>
2024-12-09 00:42:40 +0500

    Auth changes with firstname-lastname

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -22,7 +22,8 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|min:3',
+            'firstname' => 'required|min:3',
+            'lastname' => 'required|min:3',
             'email' => 'required|email|unique:users',
             'password' => 'required|confirmed'
         ];
